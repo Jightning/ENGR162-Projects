1551b8786f03c453d1558bb6d204ae48c8c0624d
Juan <jmvaz15@gmail.com>
2026-04-26 19:38:23 -0400

    full run adjustments

--- full_run.py
+++ full_run.py
@@ -23,6 +23,7 @@ sensor_left = UltrasonicSensor(24)
 IR = IRSensor(4, 5)
 IMU = IMUSensor()
 
+# Set to true during actual run
 JSON_LOG = True #  save path to a json, set to false if it errors
 CALIBRATE = True # Calibrate gyro, set to false if it causes issues
 
@@ -33,7 +34,7 @@ TIME_BASED = False # Run for only a set amount of time
 # initial variables
 SPEED = 30 # Base speed of the robot
 SLOW_SPEED = 5
-CELL_DIST = 5 # Cell distance
+CELL_DIST = 10 # Cell distance
 
 DIST_MIN = 7 # Distance considered too close in cm
 DIST_MAX = 30 # Max distance before wall isn't considered significant
@@ -57,9 +58,17 @@ Ki_move = 0
 Kd_move = 0.05
 
 run_finished = False # if the run is finished
+critical_z = 1.5 # Max acceptable z-score before a value is an outlier. 12.73% alpha, so max of 3 outliers
 
-uni_timer = 0
+# JSON style pathing, lets upload to JSON file once code terminates
+path = []
 
+x = 0
+y = 0
+direction = 0 # initial facing directions (north)
+directions = ['N', 'E', 'S', 'W']
+
+cur_time = time.time()
 
 def stop():
     motorL.stop()
@@ -94,7 +103,7 @@ def unload():
     time.sleep(2)
     stop()
 
-def get_safe_dist(sensor, default=DIST_MAX):
+def get_safe_dist(sensor, default=DIST_MAX + 1):
     try:
         dist = sensor.getDist
         # If the sensor returns None, use the default value
@@ -111,7 +120,7 @@ def calibrate_gyro(samples=200):
     s = 0.0
     print(f"Calibrating gyro... Should take {0.01 * samples:.2f} seconds")
     
-    for _ in tqdm(range(samples), desc="Calibrating", ascii=True):
+    for _ in tqdm(range(samples), desc="Calibrating"):
         gx, gy, gz = IMU.getGyro()
         s += gz
         time.sleep(0.01)
@@ -119,19 +128,83 @@ def calibrate_gyro(samples=200):
     print(f"Done! Gyro Bias: {s / samples}\n")
     return s / samples
 
+'''
 def get_target_dist(samples=200):
     s = 0.0
     print(f"Getting Target Distance (Make sure robot is centered)... Should take {0.01 * samples:.2f} seconds")
     
-    for _ in tqdm(range(samples), desc="Calibrating", ascii=True):
+    for _ in tqdm(range(samples), desc="Calibrating"):
         right_dist = sensor_right.getDist
-        if right_dist is None:
-            continue
+        while right_dist is None:
+            sensor_right.getDist
+
         s += right_dist
         time.sleep(0.01)
         
     print(f"Done! Target Dist: {s / samples}\n")
     return s / samples
+'''
+
+def get_target_dist(base_queue, samples=200):
+    s = 0.0
+    print(f"Getting Target Distance (Make sure robot is centered)... Should take ~{0.015 * samples:.2f} seconds")
+    dists = base_queue.copy()
+
+    for _ in tqdm(range(samples), desc="Calibrating"):
+        right_dist = get_safe_dist(sensor_right)
+        z = get_z_score(dists, right_dist)
+
+        while z > critical_z:
+            dists.pop(0)
+            dists.append(right_dist)
+ 
+            right_dist = get_safe_dist(sensor_right)
+            z = get_z_score(dists, right_dist)
+
+        dists.pop(0)
+        dists.append(right_dist)
+
+        s += right_dist
+        time.sleep(0.01)
+        
+    print(f"Done! Target Dist: {s / samples}\n")
+    return s / samples
+
+def form_queue(sensor, samples=10):
+    dist_queue = []
+    print(f"Creating Distance Queue... Should take {0.01 * samples:.2f} seconds")
+    for _ in tqdm(range(samples)):
+        dist = get_safe_dist(sensor)
+        dist_queue.append(dist)
+        time.sleep(0.01)
+
+    return dist_queue
+
+def get_z_score(vals, val):
+    std = 0
+    avg = sum(vals) /  len(vals)
+    for v in vals:
+        std += (v - avg) ** 2
+    std = max(math.sqrt(std / len(vals)), 1e-1)
+
+    return (val - avg) / std
+
+def log(turned=False, heat_magnitude=0.0, magnetic_magnitude=0.0):
+    path.append({
+        "pos": (x, y),
+        "dir": directions[direction],
+        "turned": turned,
+        "heat_source": abs(heat_magnitude) >= HEAT_SOURCE_MAGNITUDE,
+        "magnetic_source": abs(magnetic_magnitude) >= MAGNET_SOURCE_MAGNITUDE
+    })
+    # print(f"({x}, {y}) going {directions[direction]}")
+ 
+def update_coordinates():
+    global x, y
+    if direction == 0: y += 1 # North
+    elif direction == 1: x += 1 # East
+    elif direction == 2: y -= 1 # South
+    elif direction == 3: x -= 1 # West
 
 def turn_degrees_pid(deg=90.0, clockwise=True, tolerance=3, timeout=5.0):
     target = -float(deg) if clockwise else float(deg)
@@ -180,44 +253,31 @@ def turn_degrees_pid(deg=90.0, clockwise=True, tolerance=3, timeout=5.0):
             time.sleep(0.001)
     except KeyboardInterrupt:
         print("Done while turning")
+        global run_finished
         run_finished = True
         stop()
         return
     
     stop()
 
-def log(turned=False, heat_magnitude=0.0, magnetic_magnitude=0.0):
-    path.append({
-        "pos": (x, y),
-        "dir": directions[direction],
-        "turned": turned,
-        "heat_source": abs(heat_magnitude) >= HEAT_SOURCE_MAGNITUDE,
-        "magnetic_source": abs(magnetic_magnitude) >= MAGNET_SOURCE_MAGNITUDE
-    })
-    # print(f"({x}, {y}) going {directions[direction]}")
- 
-def update_coordinates():
-    global x, y
-    if direction == 0: y += 1 # North
-    elif direction == 1: x += 1 # East
-    elif direction == 2: y -= 1 # South
-    elif direction == 3: x -= 1 # West
+def move_one_cell(timeout=5.0):
+    global dist_queue
 
-def move_one_cell(ignore_right=False):
     prev_error = get_safe_dist(sensor_right) - TARGET_DIST
     prev_time = time.time()
-    uni_timer = time.time()
+    start_time = prev_time
     integral = 0.0
     vy, y = 0, 0
 
     try:
-        while True:
+        while time.time() - start_time <= timeout:
             front_dist = get_safe_dist(sensor_front)
-            right_dist = sensor_right.getDist
-            left_dist = sensor_left.getDist
-            #print(str(right_dist) + " vs " + str(DIST_MIN))
-            #print(str(left_dist) + " vs " + str(DIST_MIN))
-            if right_dist is None or left_dist is None or front_dist is None:
+            right_dist = get_safe_dist(sensor_right)
+
+            z_score = get_z_score(dist_queue, right_dist)
+            dist_queue.pop(0)
+            dist_queue.append(right_dist)
+            if z_score > critical_z:
                 continue
 
             cur_time = time.time()
@@ -228,154 +288,121 @@ def move_one_cell(ignore_right=False):
             vy = vy + ay * dt
             y = y + vy * dt
             
-            # Reached distance or wall
-            if abs(y) >= CELL_DIST or front_dist < DIST_MIN:
+            # Reached distance or wall, or can turn right
+            if abs(y) >= CELL_DIST or front_dist < DIST_MIN or right_dist > DIST_MAX:
                 break
-            elif front_dist < DIST_MIN:
-                time.sleep(0.01)
-                front_dist = sensor_front.getDist
-                if (front_dist is not None and front_dist < DIST_MIN):
-                    break
-                else:
-                    continue
+
+            # PID to align between the two walls
+            error = right_dist - TARGET_DIST
             
-            elif right_dist > DIST_MAX and not ignore_right:
-                time.sleep(0.01)
-                right_dist = sensor_right.getDist
-                if (right_dist is not None and right_dist > DIST_MAX):
-                    break
-                else:
-                    continue
+            integral += error * dt
+            integral = max(min(integral, 50.0), -50.0)
 
-            # No wall on the right/left, just drive straight
-            if True and (right_dist > DIST_MAX or left_dist > DIST_MAX):
-                start(SPEED)
-                prev_error = 0
-            else:
-                # PID to align between the two walls
-                error = right_dist - TARGET_DIST
-                
-                integral += error * dt
-                integral = max(min(integral, 50.0), -50.0)
+            derivative = (error - prev_error) / max(dt, 1e-4) # using last error rather than dt formula since error could reset to 0
+            derivative = max(min(derivative, 10.0), -10.0)
 
-                derivative = (error - prev_error) / max(dt, 1e-4) # using last error rather than dt formula since error could reset to 0
-                derivative = max(min(derivative, 10.0), -10.0)
-                prev_error = error
-
-                adjustment = (Kp_move * error) + (Kd_move * derivative) + (Ki_move * integral)
-                adjustment = max(min(adjustment, SPEED), -SPEED)
-                #print("to ", adjustment)
-                
-                # positive adj if turning left, negative if turning right
-                startL(SPEED + adjustment)
-                startR(SPEED - adjustment)
+            prev_error = error
+            adjustment = (Kp_move * error) + (Kd_move * derivative) + (Ki_move * integral)
+            adjustment = max(min(adjustment, MOTOR_CMD_MAX), -MOTOR_CMD_MAX)
             
-
-
-            time.sleep(0.05)
+            startL(SPEED + adjustment)
+            startR(SPEED - adjustment)
+    
+            time.sleep(0.001)
     except KeyboardInterrupt:
         print("Done while onward")
+        global run_finished
         run_finished = True
         stop()
         return
     
     stop()
 
+# Main Code starts here --------------------------------------------------------------------------
 stop()
-
 print("Loading Cargo...")
 cargoMotor.start(-50)
 time.sleep(2)
 cargoMotor.stop()
 
+# queue of the last few right distances for checking outliers
+dist_queue = form_queue(sensor_right, samples=20)
+
 if CALIBRATE:
     GYRO_BIAS = calibrate_gyro()
-    TARGET_DIST = get_target_dist()
+    TARGET_DIST = get_target_dist(dist_queue)
 else:
     GYRO_BIAS = 0.3
     TARGET_DIST = 11 # Target distance to stay from wall (dist from right sensor to right wall) (try to make slightly smaller in order to keep more right)
-
-
-# JSON style pathing, lets upload to JSON file once code terminates
-path = []
-
-x = 0
-y = 0
-direction = 0 # initial facing directions (north)
-directions = ['N', 'E', 'S', 'W']
-
-cur_time = time.time()
-
-'''if TEST:
-    while True:
-        front_dist = get_safe_dist(sensor_front)
-        right_dist = get_safe_dist(sensor_right)
-        left_dist = get_safe_dist(sensor_left)
-
-        mag_x, mag_y, mag_z = IMU.getMag()
-        mag_x, mag_y = -mag_x, -mag_y 
-
-        print(f"Front: {front_dist:.2f} Right: {right_dist:.2f} Left: {left_dist:.2f} Mag X: {mag_x:.2f} Mag Y: {mag_y:.2f} Mag Z: {mag_z:.2f}")'''
-def form_queue(sensor, samples=10):
-    dist_queue = []
-    print("Creating Distance Queue")
-    for _ in tqdm(range(samples)):
-        dist = get_safe_dist(sensor)
-        dist_queue.append(dist)
-        time.sleep(0.01)
-        
-dist_queue = form_queue(sensor_right)
+    
 
 while time.time() - cur_time < 10 or not TIME_BASED:
     try:
-        if run_finished:
-            break
-        
         front_dist = get_safe_dist(sensor_front)
         right_dist = get_safe_dist(sensor_right)
         left_dist = get_safe_dist(sensor_left)
-                
+        
+        z_score = get_z_score(dist_queue, right_dist)
+        dist_queue.pop(0)
+        dist_queue.append(right_dist)
+
+        # If the current value is an outlier, then ignore
+        if z_score > critical_z:
+            continue
+        
         # Magnetic/Heat source calculations
         mag_x, mag_y, mag_z = IMU.getMag() # Magnet
         mag_x, mag_y = -mag_x, -mag_y # IMU is flipped on the robot, so just flipping the values for clarity (+x is right, +y is forward)
         magnet_magnitude = math.sqrt(mag_x**2 + mag_y**2 + mag_z**2)
         ir_left, ir_right = IR.value1, IR.value2 # Heat
-        ir_avg=max(ir_left, ir_right)
+        ir_avg = max(ir_left, ir_right)
         
-        if ir_avg > HEAT_SOURCE_MAGNITUDE or magnet_magnitude > MAGNET_SOURCE_MAGNITUDE:
+        if ir_avg > HEAT_SOURCE_MAGNITUDE or magnet_magnitude > MAGNET_SOURCE_MAGNITUDE: # Found a source
             stop()
             print("Turning Around", right_dist, front_dist)
             turn_degrees_pid(clockwise=False, deg=180.0)
-
+            dist_queue = form_queue(sensor_right, samples=20)
             direction = (direction - 2) % 4
+
+            print("Then Onwards")
+            move_one_cell()
+            update_coordinates()
+
             log(turned=True, heat_magnitude=ir_avg, magnetic_magnitude=magnet_magnitude)
-        elif right_dist > DIST_MAX and front_dist > DIST_MAX and left_dist > DIST_MA:
-            unload()
+        elif right_dist > DIST_MAX and front_dist > DIST_MAX and left_dist > DIST_MAX: # At an open space
+            print("Open Space Found")
             break
         elif right_dist > DIST_MAX: # Turn right if clear
             start()
-            time.sleep(0.8) # TODO Go a little more forward to center
+            time.sleep(0.5) # Go a little more forward to center
             stop()
+
             print("Turning Right")
             turn_degrees_pid(clockwise=True)
+            dist_queue = form_queue(sensor_right, samples=20)
+            direction = (direction + 1) % 4
+
             print("Then Onwards")
-            move_one_cell(ignore_right=True)
+            move_one_cell()
             
             update_coordinates()
-            direction = (direction + 1) % 4
             log(turned=True, heat_magnitude=ir_avg, magnetic_magnitude=magnet_magnitude)
         elif front_dist < DIST_MIN: # Turn left if unable to go forward or turn right
             stop()
             print("Turning Left", right_dist, front_dist)
             turn_degrees_pid(clockwise=False)
+            dist_queue = form_queue(sensor_right, samples=20)
+            direction = (direction + 1) % 4
 
-            direction = (direction - 1) % 4
             log(turned=True, heat_magnitude=ir_avg, magnetic_magnitude=magnet_magnitude)
         else: # Travel one cell forward if clear
             print("Onward")
             move_one_cell()
             update_coordinates()
             log(heat_magnitude=ir_avg, magnetic_magnitude=magnet_magnitude)
+
+        if run_finished:
+            break
 
         time.sleep(0.01)
     except KeyboardInterrupt:
---
+++ map_reader.py
@@ -0,0 +1,1 @@
+with open('maze.json', 'w') as file:
